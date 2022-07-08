a24acc652c7284d927267f68e67ff5139cafa0dd
ivan_semenov <semenofiv@gmail.com>
2022-07-08 15:26:33 +0500

    create app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django import forms
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    text = models.TextField()
+    description = models.CharField(max_length=300)
+    Image = models.CharField(max_length=1000)
+
+
